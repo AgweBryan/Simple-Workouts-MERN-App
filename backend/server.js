7dbe9f648c627037a54f66c1c0aa0aa76c11199e
AgweBryan <agwebryanmunakwa@gmail.com>
2023-03-31 15:43:29 +0100

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
+const cors = require("cors");
 
 const app = express();
 
 // parse request body
 app.use(express.json());
+
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
